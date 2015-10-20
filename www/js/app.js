04691a7ea49df1741736a1d07744eace3497b6ff
unknown <rajeshg@OMCL764-DDQKP32.omnicell.com>
2015-10-20 05:49:44 -0700

    basic login page and two page navigation

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,52 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('nix', ['ionic'])
+.controller('LogInCtrl', function($scope, $state, formData) {
+ $scope.user = {};
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+ $scope.submitForm = function(user) {
+   if (user.userId && user.password) {
+	 formData.updateForm(user);
+     console.log("Logging In ", formData.getForm());
+	 $state.go('hello');
+   } else {
+     alert("Please enter user name and password");
+   }
+ };
 })
+
+.controller('HelloCtrl', function($scope, $state,formData) {
+	$scope.user = formData.getForm();
+	$scope.logout = function() {
+		 console.log("Logging Out ", $scope.user);
+		 $state.go('login');		 
+	};
+})
+
+.service('formData', function() {
+ return {
+   form: {},
+   getForm: function() {
+     return this.form;
+   },
+   updateForm: function(form) {
+     this.form = form;
+   }
+ }
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  .state('login', {
+    url: "/login",
+    templateUrl: "templates/login.html",
+    controller: 'LogInCtrl'
+  })
+.state('hello', {
+   url: "/hello",
+   templateUrl: "templates/hello.html",
+   controller: 'HelloCtrl'
+ })
+ $urlRouterProvider.otherwise('login');
+});
