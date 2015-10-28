18e4508905924f7a2e3281055f9b491b98345c40
unknown <rajeshg@OMCL764-DDQKP32.omnicell.com>
2015-10-28 06:39:42 -0700

    getting jwt access token

--- www/js/app.js
+++ www/js/app.js
@@ -4,13 +4,28 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('nix', ['ionic'])
-.controller('LogInCtrl', function($scope, $state, formData) {
+ .constant('urls', {
+       BASE: 'http://jwt.dev:8000',      
+	   TOKEN_REQUEST: 'http://localhost:40405/oauth/token'
+   })
+.controller('LogInCtrl', function($scope, $state, $http, formData, Auth) {
  $scope.user = {};
-
+	
  $scope.submitForm = function(user) {
+	
    if (user.userId && user.password) {
-	 formData.updateForm(user);
-     console.log("Logging In ", formData.getForm());
+	 formData.updateForm(user);	 
+	 $http({
+			method: 'POST',
+			url: 'http://localhost:40405/oauth/token',
+			headers: {'OCClientContext': '{   "ProductName" : "CP",   "PartnerProductId" : "",   "OmniCenterInstallation" : "CPC01",   "TimeStamp" : "06/26/2015 19:40:05"  }', 'Content-Type': 'application/x-www-form-urlencoded'},
+			data: 'UserName='+ encodeURIComponent($scope.user.userId)+'&Password='+ encodeURIComponent($scope.user.password) + '&grant_type=password'
+		}).then(function successCallback(response) {console.log(response.data)
+			window.localStorage['token'] = response.data.access_token;
+			console.log(Auth.getTokenClaims());
+		},
+				function errorCallback(response) {console.log(response.data)});
+     console.log("Logging In ", user);
 	 $state.go('hello');
    } else {
      alert("Please enter user name and password");
@@ -18,7 +33,7 @@ angular.module('nix', ['ionic'])
  };
 })
 
-.controller('HelloCtrl', function($scope, $state, $http,formData) {
+.controller('HelloCtrl', function($scope, $state, $http, formData) {
 	$scope.user = formData.getForm();
 	$scope.logout = function() {
 		 console.log("Logging Out ", $scope.user);
@@ -45,7 +60,54 @@ angular.module('nix', ['ionic'])
    }
  }
 })
+.factory('Auth', ['$http', 'urls', function() {
+       function urlBase64Decode(str) {
+           var output = str.replace('-', '+').replace('_', '/');
+           switch (output.length % 4) {
+               case 0:
+                   break;
+               case 2:
+                   output += '==';
+                   break;
+               case 3:
+                   output += '=';
+                   break;
+               default:
+                   throw 'Illegal base64url string!';
+           }
+           return window.atob(output);
+       }
 
+       function getClaimsFromToken() {
+           var token =  window.localStorage['token'];
+           var user = {};
+           if (typeof token !== 'undefined') {
+               var encoded = token.split('.')[1];
+               user = JSON.parse(urlBase64Decode(encoded));
+           }
+           return user;
+       }
+
+       var tokenClaims = getClaimsFromToken();
+
+       return {
+           signup: function (data, success, error) {
+               $http.post(urls.BASE + '/signup', data).success(success).error(error)
+           },
+           login: function (data, success, error) {
+               $http.post(urls.BASE + '/signin', data).success(success).error(error)
+           },
+           logout: function (success) {
+               tokenClaims = {};
+               window.localStorage['token']= '';
+               success();
+           },
+           getTokenClaims: function () {
+               return tokenClaims;
+           }		   
+       };
+   }]
+ )
 .config(function($stateProvider, $urlRouterProvider){
   $stateProvider
   .state('login', {
