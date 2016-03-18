357f5dde82213d4b7fba67da12a80ed5be8505ac
NAMRATA BUDDHADEV /6006 <namratab@omnicell.com>
2016-03-17 19:13:40 -0700

    enter fix

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('nix', ['ionic','nix.controllers', 'nix.services', 'nix.routes', 'nix.constants', 'ngCordova','nix.numeric'])
+angular.module('nix', ['ionic','nix.controllers', 'nix.services', 'nix.routes', 'nix.constants', 'ngCordova','nix.numeric', 'nix.directives'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -19,8 +19,22 @@ angular.module('nix', ['ionic','nix.controllers', 'nix.services', 'nix.routes', 
   });
 })
 
-angular.module('nix.controllers', [])
+angular.module('nix.directives', [])
+.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
 
+angular.module('nix.controllers', [])
 
 angular.module('nix.controllers')
 .controller('menuCtrl', function($scope, $state, httpService, auth, $rootScope) {
@@ -53,3 +67,4 @@ angular.module('nix.services', [])
    }
  }
 })
+
