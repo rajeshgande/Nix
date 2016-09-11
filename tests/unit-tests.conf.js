7a2a1373e18bb5fab1a301d999630675db8dd164
rajeshg <rajesh.gande@omnicell.com>
2016-09-11 12:26:56 -0700

    added additional test for cycle count controller and also added karma code coverage plugin

--- tests/unit-tests.conf.js
+++ tests/unit-tests.conf.js
@@ -38,14 +38,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '../www/js/controller.cyclecount.js': ['coverage']
     },
 
+ 
     // plugins: ['karma-spec-reporter'],
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage'],
+
+     coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
--- www/js/controller.cyclecount.js
+++ www/js/controller.cyclecount.js
@@ -1,37 +1,36 @@
 angular.module('nix.controllers')
-.controller('CycleCountCtrl', function($scope, httpService, $cordovaBarcodeScanner, $ionicPlatform, $ionicPopup) {
+.controller('CycleCountCtrl', function($scope, httpService, $ionicPopup, $cordovaBarcodeScanner, $ionicPlatform, $window) {
 	 var vm = this;
+
+     vm.setCpSelection = function(cp) {
+            vm.selectedCp = cp; 
+            window.localStorage['SelectedCP'] = vm.selectedCp;
+            window.localStorage['omnisiteid'] = vm.selectedCp.OmniSiteId;
+            window.localStorage['omniIpAddress'] = vm.selectedCp.IpAddress;                
+     };
 
      httpService.getAllCPs().then(function(data) {
 		  vm.cps=data;
-          vm.selectedCp =  data[0]; 
-          window.localStorage['SelectedCP'] = vm.selectedCp;
-          window.localStorage['omnisiteid'] = vm.selectedCp.OmniSiteId;
-          window.localStorage['omniIpAddress'] = vm.selectedCp.IpAddress;
+          vm.setCpSelection(data[0]);
+         
 		});
            
 	vm.item = { ItemId : "", FormattedGenericName : "", QuantityOnHand : "", ExpirationDate : "", Location : "", ItemBarCode : "" };
    
-    vm.setCpSelection = function(cp) {
-        window.localStorage['SelectedCP'] = cp;
-        window.localStorage['omnisiteid'] = cp.OmniSiteId;
-        window.localStorage['omniIpAddress'] = cp.IpAddress;                
-     };
+    
 
     // vm.selectedCp =  JSON.parse(window.localStorage['SelectedCP']);
     //console.log( vm.selectedCp);
-
-    if (!window.cordova)
+    vm.isRunningInBrowser = false;
+    if (!$window.cordova)
     {
-        console.log("runnign i ndev")
-        // running in dev browser mode
+        console.log("runnign in dev mode")
         vm.item.ItemBarCode = '1234567';
-        // vm.item.ItemBarCode = '5026859315';
+        vm.isRunningInBrowser = true;        
     }
     		 
-    vm.submitForm = function(item) {		
-		 formData.updateForm(item);	 
-		 httpService.updateQty(item);
+    vm.perfomCycleCount = function() {		 	 
+		 httpService.updateQty(vm.item);
 	 };
      
      vm.getitem = function(){   
@@ -46,36 +45,35 @@ angular.module('nix.controllers')
                 }); 
         };
         
-     if (vm.currentlyScanning === true) {           
-            return;
-      }
-     else if (!window.cordova) {
-         // running in dev browser mode
-        vm.currentlyScanning = false;        
-        vm.scan =  vm.getitem;
-    } else {
-        vm.currentlyScanning = true;
-        // running in mobile device  
-        vm.scan = function(){
-            $ionicPlatform.ready(function() {
-                $cordovaBarcodeScanner
-                .scan()
-                .then(function(result) {
-                    vm.currentlyScanning = false;
-                    vm.item = { ItemId : "", FormattedGenericName : "", QuantityOnHand : "", ExpirationDate : "", Location : "", ItemBarCode : "" };
-                    vm.item.ItemBarCode = result.text;
-                    console.log("Scanned barcode: " + vm.item.ItemBarCode);                    
-                    httpService
-                        .getItemDetails(vm.item.ItemBarCode)
-                        .then(function(data) {
-                            vm.item=data;
-                            console.log(data);
-                            });                    
-                }, function(error) {
-                    $ionicPopup.alert({ title: 'Error: ' + error });
-                    console.log('Error: ' + error);
+    vm.scan = function(){
+        if (vm.currentlyScanning === true) {           
+                    return;
+            }
+            else if (vm.isRunningInBrowser) {
+                vm.currentlyScanning = false;        
+                vm.getitem();
+            } else {
+                vm.currentlyScanning = true;
+                console.log('running in mobile device');      
+                $ionicPlatform.ready(function() {
+                    $cordovaBarcodeScanner
+                    .scan()
+                    .then(function(result) {
+                        vm.currentlyScanning = false;
+                        vm.item = { ItemId : "", FormattedGenericName : "", QuantityOnHand : "", ExpirationDate : "", Location : "", ItemBarCode : "" };
+                        vm.item.ItemBarCode = result.text;
+                        console.log("Scanned barcode: " + vm.item.ItemBarCode);                    
+                        httpService
+                            .getItemDetails(vm.item.ItemBarCode)
+                            .then(function(data) {
+                                vm.item=data;
+                                console.log(data);
+                                });                    
+                    }, function(error) {
+                        $ionicPopup.alert({ title: 'Error: ' + error });
+                        console.log('Error: ' + error);
+                    });
                 });
-            });
-        };
-    }
+            }
+    };
 })
