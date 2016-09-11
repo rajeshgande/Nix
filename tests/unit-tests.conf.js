7606700c0ee0c86a87c064553c007936d4009900
rajeshg <rajesh.gande@omnicell.com>
2016-09-11 12:34:06 -0700

    addes services js file to coverage list

--- tests/unit-tests.conf.js
+++ tests/unit-tests.conf.js
@@ -38,7 +38,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '../www/js/controller.cyclecount.js': ['coverage']
+      '../www/js/controller.*.js': ['coverage'],
+      '../www/js/services.*.js': ['coverage']
     },
 
  
